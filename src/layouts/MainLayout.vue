c7d9fdb340cb8d8069c3ab762060963be4acd616
Declan Possnett <declanpossnett@gmail.com>
2025-10-14 20:37:51 +0100

    create responsive header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -14,64 +14,105 @@
     view="hHh lpR fFf"
   >
     <q-header
-      elevated
-      class="bg-primary text-white"
+      class="bg-white text-primary"
       height-hint="98"
     >
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-item
-            clickable
-            tag="a"
-            href="/"
-          >
-            <q-avatar>
-              <img
-                src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
-              />
-            </q-avatar>
-            &nbsp;Title
-          </q-item>
-        </q-toolbar-title>
+      <div>
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-item
+              clickable
+              tag="a"
+              href="/"
+              class="flex items-center"
+              style="padding: 0.5rem 0"
+            >
+              <q-avatar>
+                <img src="compass-logo.png" />
+              </q-avatar>
 
-        <q-btn
-          class="MainLayout-mobileMenu"
-          dense
-          flat
-          round
-          icon="menu"
-          @click="toggleMobileMenu"
-        />
-      </q-toolbar>
+              <div class="MainLayout-titleContainer">
+                <h1>COMPASS</h1>
+                <h2>BUILDING CONTRACTORS</h2>
+              </div>
+            </q-item>
+          </q-toolbar-title>
 
-      <q-tabs
-        class="MainLayout-desktopMenu"
-        align="right"
-      >
-        <q-route-tab
-          to="/about"
-          label="ABOUT"
-        />
-        <q-route-tab
-          to="/services"
-          label="SERVICES"
-        />
-        <q-route-tab
-          to="/projects"
-          label="PROJECTS"
-        />
-        <q-route-tab
-          to="/contact"
-          label="CONTACT"
-        />
-      </q-tabs>
+          <div class="MainLayout-desktopContactInfo">
+            <div>
+              <a
+                href="tel:01775670045"
+                target="_blank"
+              >
+                <q-icon name="phone" />
+                <span>01775 670045</span>
+              </a>
+              <a
+                href="tel:07368957257"
+                target="_blank"
+              >
+                <q-icon name="phone" />
+                <span>07368957257</span>
+              </a>
+            </div>
+            <a
+              href="mailto:compass.buildingcontractors@gmail.com"
+              target="_blank"
+            >
+              <q-icon name="email" />
+              <span>compass.buildingcontractors@gmail.com</span>
+            </a>
+          </div>
+
+          <q-btn
+            class="MainLayout-mobileMenuButton"
+            dense
+            flat
+            round
+            icon="menu"
+            style="font-size: 1.5rem"
+            @click="toggleMobileMenu"
+          />
+        </q-toolbar>
+
+        <q-tabs
+          class="MainLayout-desktopMenu"
+          align="right"
+        >
+          <q-route-tab
+            to="/about"
+            label="ABOUT"
+          />
+          <q-route-tab
+            to="/services"
+            label="SERVICES"
+          />
+          <q-route-tab
+            to="/projects"
+            label="PROJECTS"
+          />
+          <q-route-tab
+            to="/contact"
+            label="CONTACT"
+          />
+        </q-tabs>
+      </div>
     </q-header>
 
     <q-drawer
-      show-if-above
+      class="MainLayout-mobileMenu"
+      :show-if-above="false"
       v-model="mobileMenuOpen"
       side="right"
     >
+      <q-btn
+        dense
+        flat
+        round
+        icon="close"
+        style="font-size: 1.5rem"
+        @click="toggleMobileMenu"
+      />
       <q-list style="min-width: 100px">
         <q-item
           clickable
@@ -112,20 +153,174 @@
       elevated
       class="bg-grey-8 text-white"
     >
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
+      <div>
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="/compass-logo-white.png" />
+            </q-avatar>
+          </q-toolbar-title>
+        </q-toolbar>
+      </div>
     </q-footer>
   </q-layout>
 </template>
 
 <style lang="scss" scoped>
   .MainLayout {
+    header {
+      border-bottom: 5px solid $secondary;
+
+      > div {
+        margin: auto;
+        max-width: 1080px;
+      }
+    }
+
+    footer {
+      > div {
+        margin: auto;
+        max-width: 1080px;
+      }
+    }
+
+    .q-toolbar__title {
+      display: block;
+
+      @include respond-to('sm') {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      @media screen and (min-width: 580px) {
+        display: block;
+        margin-top: 1rem !important;
+        margin-bottom: -1.5rem;
+      }
+    }
+
+    .q-avatar {
+      height: 5rem;
+      width: auto;
+      margin-left: 0rem;
+
+      @include respond-to('sm') {
+        height: 6rem;
+        margin-left: -0.5rem;
+      }
+    }
+
+    &-titleContainer {
+      display: flex;
+      flex-direction: column;
+      justify-items: center;
+      height: 100%;
+      gap: 0.25rem;
+
+      @include respond-to('sm') {
+        align-items: center;
+      }
+
+      > h1,
+      h2 {
+        display: none;
+
+        @media screen and (min-width: 320px) {
+          display: flex;
+        }
+      }
+    }
+
+    &-desktopContactInfo {
+      display: none;
+
+      @media screen and (min-width: 580px) {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-items: center;
+        margin-top: 2rem;
+        gap: 0.25rem;
+
+        > div {
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+        }
+
+        a {
+          text-decoration: none;
+          color: $primary;
+
+          span {
+            text-decoration: underline;
+          }
+        }
+
+        i {
+          color: $secondary;
+          margin-right: 0.25rem;
+        }
+      }
+    }
+
+    :deep(.MainLayout-mobileMenu) {
+      padding: 0.9rem;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      color: $primary;
+      fill: $primary;
+
+      .q-list {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-top: 2rem;
+        font-size: 1.5rem;
+        gap: 2rem;
+
+        .q-tab__label {
+          font-weight: bold !important;
+        }
+      }
+    }
+
+    h1 {
+      font-size: 1.9rem;
+      line-height: 1.5rem;
+      font-weight: bold;
+      margin: 0;
+
+      @include respond-to('sm') {
+        font-size: 2.1rem;
+        line-height: 2.1rem;
+      }
+
+      @include respond-to('md') {
+        font-size: 2.56rem;
+        line-height: 2.56rem;
+      }
+    }
+
+    h2 {
+      font-weight: bold;
+      margin: 0;
+      font-size: 0.75rem;
+      line-height: 0.75rem;
+
+      @include respond-to('sm') {
+        font-size: 0.8rem;
+        line-height: 0.8rem;
+      }
+
+      @include respond-to('md') {
+        font-size: 1rem;
+        line-height: 1rem;
+      }
+    }
+
     &-desktopMenu {
       display: none;
 
@@ -134,7 +329,7 @@
       }
     }
 
-    &-mobileMenu {
+    &-mobileMenuButton {
       display: block;
 
       @include respond-to('sm') {
