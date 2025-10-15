19e0928799cdd6aa79f87d35357d3edfcb1046ab
Declan Possnett <declanpossnett@gmail.com>
2025-10-15 09:44:02 +0100

    create content section

---
+++ src/components/ContentSection.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+  export interface ContentSectionProps {
+    sectionTitle: string
+    paragraphs: string[]
+    dark?: boolean
+  }
+
+  withDefaults(defineProps<ContentSectionProps>(), {
+    sectionTitle: '',
+    paragraphs: () => [''],
+    dark: true,
+  })
+</script>
+
+<template>
+  <div
+    class="ContentSection"
+    :class="dark ? 'dark' : 'light'"
+  >
+    <h2>{{ sectionTitle }}</h2>
+
+    <p
+      v-for="(paragraph, i) in paragraphs"
+      :key="i"
+    >
+      {{ paragraph }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .ContentSectioon {
+    &.dark {
+      background: $primary;
+      color: #fff;
+    }
+
+    &.light {
+      background: #fff;
+      color: $primary;
+    }
+  }
+</style>
